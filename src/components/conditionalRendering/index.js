56fcac3dfa837022f75c0eecf1f48954b4da44a3
AlexVovan <alehnovich.vova@gmail.com>
2021-12-06 15:57:31 +0300

    add conditional rendering

---
+++ src/components/conditionalRendering/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class sayHi extends Component {
+    state = {
+        loading: false,
+        greeting: ""
+    };
+
+    sayHi = () => {
+        this.setState({ loading: false, greeting: "Hello!!!" });
+    };
+
+    runLoader = () => {
+        setTimeout(this.sayHi, 3000);
+        this.setState({ loading: true });
+    };
+
+    render() {
+        return (
+            <>
+                <button onClick={this.runLoader}>Say Hi</button>
+                {this.state.loading && <div className="loader"></div>}
+                <span>{this.state.greeting}</span>
+            </>
+        );
+    }
+
+}
+
+export default sayHi
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Counter from './components/counterComponent'
 import ButtonRepeat from './components/cyclicButton'
+import Conditional from './components/conditionalRendering'
 
 ReactDOM.render(
   <React.StrictMode>
     <Counter />
     <ButtonRepeat className="btnRepeat" />
+    <Conditional />
   </React.StrictMode>,
   document.getElementById('root')
 );
